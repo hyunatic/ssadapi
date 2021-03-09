ca1268f2aa59dac203d028c8f2dfb817a6233d96
hyunatic <habbo_armageddon@hotmail.com>
2021-03-09 23:17:06 +0800

    Professor side of api is done

--- src/routes/game.php
+++ src/routes/game.php
@@ -51,4 +51,61 @@ $app->post('/api/student/submission', function(Request $request, Response $respo
     
 });
 
+
+$app->post('/api/submission', function(Request $request, Response $response){
+    $name = $request->getParam('name');
+    $image = $request->getParam('image');
+    $date = $request->getParam('date');
+    $tutgrp = $request->getParam('tutgrp');
+    $tutid = $request->getParam('tutid');
+    $studid = $request->getParam('studid');
+    $score = "No score";
+    $comment = "No comment";
+
+    $sql = "INSERT INTO leaderboard (name, image, date, score, tutgrp, tutid, comment, studid) VALUES
+    (:name,:image,:date,:score,:tutgrp,:tutid,:comment,:studid)";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':image',  $image);
+        $stmt->bindParam(':date',   $date);
+        $stmt->bindParam(':score',   $score);
+        $stmt->bindParam(':tutgrp',   $tutgrp);
+        $stmt->bindParam(':tutid',   $tutid);
+        $stmt->bindParam(':comment',   $comment);
+        $stmt->bindParam(':studid',   $studid);
+
+        $stmt->execute();
+
+        echo '[{"response": "Student Submission is Successful"}]';
+
+    } catch(PDOException $e){
+        echo '[{"error": {"text": '.$e->getMessage().'}]';
+    }
+});
+
+$app->post('/api/marking/update', function(Request $request, Response $response){
+    $id = $request->getParam('id');
+    $score = $request->getParam('score');
+    $comment = $request->getParam('comment');
+
+    $tsql = "UPDATE leaderboard SET score = '$score', comment = '$comment' WHERE id = '$id'";
+
+    $db = new db();
+    // Connect
+    $db = $db->connect();
+    $stmt = $db->prepare($tsql);
+    $stmt->execute();
+
+    echo '[{"response": "Update Scores Successfully"}]';
+    
+});
+
 ?>
