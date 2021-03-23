b9f69b659ed8544eda131855bb082a25104a234c
hyunatic <habbo_armageddon@hotmail.com>
2021-03-24 01:27:25 +0800

    Challenger

--- src/routes/game.php
+++ src/routes/game.php
@@ -160,7 +160,7 @@ $app->post('/api/start/compete', function(Request $request, Response $response){
 $app->post('/api/get/challengers', function(Request $request, Response $response){
     $competitorid = $request->getParam("competitorid");
 
-    $tsql = "";
+    $tsql = "Select userlogin.name, leaderboard.score, tutorial.tutname, competiton.competitionid from competiton, userlogin, tutorial, leaderboard Where competiton.competitorid = '$competitorid' AND competiton.requestorid = userlogin.id AND competiton.tutid = tutorial.tutid AND competiton.leaderboardid = leaderboard.id";
 
     $db = new db();
     // Connect
