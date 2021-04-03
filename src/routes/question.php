b79ef2f8a71c816e450e44366ce80ea9ce1a2793
hyunatic <habbo_armageddon@hotmail.com>
2021-04-03 18:45:37 +0800

    sd

--- src/routes/question.php
+++ src/routes/question.php
@@ -2,9 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-function sqlGetConnection()
-{    
- 
+function sqlGetConnection(){    
     $servername = 'asedb.mysql.database.azure.com';
     $username = 'aseadmin@asedb';
     $password = 'Pa$$w0rd';
@@ -19,8 +17,7 @@ function sqlGetConnection()
 	return $conn;
 }	
 
-function sqlQuery($conn, $sql, $params = [])
-{
+function sqlQuery($conn, $sql, $params = []){
     $getResults = $conn->prepare($sql);
     $getResults->execute($params);
     $results = $getResults->fetchAll(PDO::FETCH_ASSOC);
@@ -44,11 +41,14 @@ $app->post('/api/add/tutquest', function(Request $request, Response $response){
     $section = $request->getParam('section');
     $hint = $request->getParam('hint');
 
+    $cleansol = json_encode($solution);
+    $cleanhint = json_encode($hint);
+
     $sql = "INSERT INTO quest (question, tutgrp, tutid, solution,level,section, hint) VALUES
     (:question,:tutgrp,:tutid, :solution, :level, :section, :hint)";
 
     try{
-        // Get DB Object
+        //Get DB Object
         $db = new db();
         // Connect
         $db = $db->connect();
@@ -58,22 +58,13 @@ $app->post('/api/add/tutquest', function(Request $request, Response $response){
         $stmt->bindParam(':question', $question);
         $stmt->bindParam(':tutgrp',  $tutgrp);
         $stmt->bindParam(':tutid',   $tutid);
-        $stmt->bindParam(':solution',   $solution);
+        $stmt->bindParam(':solution',   $cleansol);
         $stmt->bindParam(':level',   $level);
         $stmt->bindParam(':section',   $section);
-        $stmt->bindParam(':hint',   $hint);
-
-        $stmt->execute();
-
-        $tsql1 = "SELECT * From quest ORDER BY questid DESC LIMIT 1";
-        $stmt = $db->prepare($tsql1);
-        $stmt->execute();
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt->bindParam(':hint',   $cleanhint);
 
         $stmt->execute();
        
-        echo json_encode($results);
-
     } catch(PDOException $e){
         echo '[{"error": {"text": '.$e->getMessage().'}]';
     }
@@ -118,15 +109,25 @@ $app->post('/api/show/tutquest', function(Request $request, Response $response){
 $app->post('/api/show/unity/tutquest', function(Request $request, Response $response){
     $tutid = $request->getParam('tutid');
 
-    $tsql = "SELECT questid, question, tutgrp, tutid, solution From quest WHERE tutid = $tutid";
+    $tsql = "SELECT questid, question, tutgrp, tutid, solution, hint From quest WHERE tutid = $tutid";;
 
-    $db = new db();
-    // Connect
-    $db = $db->connect();
-    $stmt = $db->prepare($tsql);
-    $stmt->execute();
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($results);
+    $conn = sqlGetConnection();
+    $result = mysqli_query($conn,$tsql);
+    $totalrecord = mysqli_num_rows($result);
+    $counter = 0;
+
+    $output = '[';
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){
+           if(++$counter == $totalrecord){
+                $output .= '{'. '"questid":'. '"'.$row['questid'] . '"' .',"question":'. '"'.$row['question'] . '"' .',"tutgrp":'. '"'.$row['tutgrp'] . '"' .',"tutid":'. '"'.$row['tutid'] . '"' . ',"solution":'. $row['solution'] .',"hint":' .$row['hint'] . '}]';
+           }
+           else{
+                $output .= '{'. '"questid":'. '"'.$row['questid'] . '"' .',"question":'. '"'.$row['question'] . '"' .',"tutgrp":'. '"'.$row['tutgrp'] . '"' .',"tutid":'. '"'.$row['tutid'] . '"' . ',"solution":' .$row['solution'] . ',"hint":' .$row['hint'] .'},';
+           }
+        }
+    }
+    echo $output;
 });
 
 
