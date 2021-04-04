ee9bfb66518063d4cb552e354b43285be81cfd3b
hyunatic <habbo_armageddon@hotmail.com>
2021-04-04 13:34:58 +0800

    sd

--- src/routes/question.php
+++ src/routes/question.php
@@ -3,45 +3,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 
-
-$app->post('/api/add/tutquest', function(Request $request, Response $response){
-    $tutid = $request->getParam('tutid');
-    $tutgrp = $request->getParam('tutgrp');
-    $question = $request->getParam('question');
-    $solution = $request->getParam('solution');
-    $level = $request->getParam('level');
-    $section = $request->getParam('section');
-    $hint = $request->getParam('hint');
-
-    $cleansol = json_encode($solution);
-    $cleanhint = json_encode($hint);
-
-    $sql = "INSERT INTO quest (question, tutgrp, tutid, solution,level,section, hint) VALUES
-    (:question,:tutgrp,:tutid, :solution, :level, :section, :hint)";
-
-    try{
-        //Get DB Object
-        $db = new db();
-        // Connect
-        $db = $db->connect();
-
-        $stmt = $db->prepare($sql);
-
-        $stmt->bindParam(':question', $question);
-        $stmt->bindParam(':tutgrp',  $tutgrp);
-        $stmt->bindParam(':tutid',   $tutid);
-        $stmt->bindParam(':solution',   $cleansol);
-        $stmt->bindParam(':level',   $level);
-        $stmt->bindParam(':section',   $section);
-        $stmt->bindParam(':hint',   $cleanhint);
-
-        $stmt->execute();
-
-    } catch(PDOException $e){
-        echo '[{"error": {"text": '.$e->getMessage().'}]';
-    }
-});
-
 $app->post('/api/delete/tutquest', function(Request $request, Response $response){
     $questid = $request->getParam('questid');
     $sql = "DELETE from quest WHERE questid = :questid";
