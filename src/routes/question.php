a7a3a8a7f7dcf949ca2e2969aab5ef71ea04f8f0
hyunatic <habbo_armageddon@hotmail.com>
2021-04-04 14:30:03 +0800

    Fixed question

--- src/routes/question.php
+++ src/routes/question.php
@@ -2,6 +2,22 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+function sqlGetConnection()
+{    
+    $servername = 'asedb.mysql.database.azure.com';
+    $username = 'aseadmin@asedb';
+    $password = 'Pa$$w0rd';
+    $dbname = 'ssad';
+    
+    try{
+    $conn = new mysqli($servername,$username,$password,$dbname);
+    }
+    catch(Exception $e)
+    {
+        die(print_r($e->getMessage() ) );
+    }
+	return $conn;
+}
 
 $app->post('/api/delete/tutquest', function(Request $request, Response $response){
     $questid = $request->getParam('questid');
@@ -48,8 +64,6 @@ $app->post('/api/add/web/tutquest', function(Request $request, Response $respons
     $section = $request->getParam('section');
     $hint = $request->getParam('hint');
 
-    $cleansol = json_encode($solution);
-    $cleanhint = json_encode($hint);
 
     $sql = "INSERT INTO quest (question, tutgrp, tutid, solution,level,section, hint) VALUES
     (:question,:tutgrp,:tutid, :solution, :level, :section, :hint)";
@@ -65,25 +79,37 @@ $app->post('/api/add/web/tutquest', function(Request $request, Response $respons
         $stmt->bindParam(':question', $question);
         $stmt->bindParam(':tutgrp',  $tutgrp);
         $stmt->bindParam(':tutid',   $tutid);
-        $stmt->bindParam(':solution',   $cleansol);
+        $stmt->bindParam(':solution',   $solution);
         $stmt->bindParam(':level',   $level);
         $stmt->bindParam(':section',   $section);
-        $stmt->bindParam(':hint',   $cleanhint);
+        $stmt->bindParam(':hint',   $hint);
 
         $stmt->execute();
        
         $tsql1 = "SELECT * From quest ORDER BY tutid DESC LIMIT 1";
-        $stmt = $db->prepare($tsql1);
-        $stmt->execute();
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        $stmt->execute();
-       
-        echo json_encode($results);
+        $conn = sqlGetConnection();
+        $result = mysqli_query($conn,$tsql1);
+        $totalrecord = mysqli_num_rows($result);
+        $counter = 0;
 
-    } catch(PDOException $e){
-        echo '[{"error": {"text": '.$e->getMessage().'}]';
-    }
+        // | in the back the JSON is special for this function only
+        $output = '[';
+        if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_assoc($result)){
+            if(++$counter == $totalrecord){
+                    $output .= '{'. '"questid":'. '"'.$row['questid'] . '"' .',"question":'. '"'.$row['question'] . '"' .',"level":'. '"'.$row['level'] . '"' .',"section":'. '"'.$row['section'] . '"' .',"tutgrp":'. '"'.$row['tutgrp'] . '"' .',"tutid":'. '"'.$row['tutid'] . '"' . ',"solution":'. $row['solution'] .',"hint":' .$row['hint'] .'}]';
+            }
+            else{
+                    $output .= '{'. '"questid":'. '"'.$row['questid'] . '"' .',"question":'. '"'.$row['question'] . '"' .',"level":'. '"'.$row['level'] . '"' .',"section":'. '"'.$row['section'] . '"' .',"tutgrp":'. '"'.$row['tutgrp'] . '"' .',"tutid":'. '"'.$row['tutid'] . '"' . ',"solution":' .$row['solution'] .',"hint":' .$row['hint'] .'},';
+            }
+            }
+        }
+        echo $output;
+        } 
+        catch(PDOException $e){
+            echo '[{"error": {"text": '.$e->getMessage().'}]';
+        }
 });
 
 
